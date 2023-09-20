b5aa77d0a56654abc98f484f4fc4fe4abbe5a8fa
ulf <ulfahammar@gmail.com>
2023-09-20 15:49:33 +0200

    remove fzf options feature which bugged for me

--- fzf-url.sh
+++ fzf-url.sh
@@ -4,17 +4,6 @@
 #    Email: wenxuangm@gmail.com
 #  Created: 2018-04-06 12:12
 #===============================================================================
-get_fzf_options() {
-    local fzf_options
-    local fzf_default_options='-d 35% -m -0 --no-preview --no-border'
-    fzf_options="$(tmux show -gqv '@fzf-url-fzf-options')"
-    [ -n "$fzf_options" ] && echo "$fzf_options" || echo "$fzf_default_options"
-}
-
-fzf_filter() {
-  eval "fzf-tmux $(get_fzf_options)"
-}
-
 custom_open=$3
 open_url() {
     if [[ -n $custom_open ]]; then 
@@ -54,7 +43,7 @@ items=$(printf '%s\n' "${urls[@]}" "${wwws[@]}" "${gh[@]}" "${ips[@]}" "${gits[@
 )
 [ -z "$items" ] && tmux display 'tmux-fzf-url: no URLs found' && exit
 
-fzf_filter <<< "$items" | awk '{print $2}' | \
+fzf-tmux <<< "$items" | awk '{print $2}' | \
     while read -r chosen; do
         open_url "$chosen" &>"/tmp/tmux-$(id -u)-fzf-url.log"
     done
